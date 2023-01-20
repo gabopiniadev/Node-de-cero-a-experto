97ae36d23396f7edcaadcf5aa2c537e14007eeb5
Gabriel Alejandro Piña <gabrielpinia@hotmail.com>
2023-01-20 00:06:10 -0400

    curso

--- 1-bases-node/app.js
+++ 1-bases-node/app.js
@@ -1,9 +1,27 @@
+let fs = require('fs');
 
-for(let i = 0; i <= 10; i++) {
-    let value = 5*i;
-    console.log("5 x " , i ,  "es = " , value);
+console.clear();
+console.log("==============================");
+console.log("Tabla de Multiplicar");
+console.log("==============================");
+
+let salida = '';
+let multiplo = '';
+
+for(let z = 1; z <= 10; z++) {
+
+    multiplo = z;
+
+        for(let i = 0; i <= 10; i++) {
+            salida = `${multiplo} x ${i} = ${multiplo * i}`; 
+            console.log(salida);
+        
+
+        fs.writeFile( `tabla-${multiplo}.txt`, salida, (err) => {
+            if(err) throw err;
+            console.log("Documento Creado!");
+        });
+    }
 }
-
-
 
 
